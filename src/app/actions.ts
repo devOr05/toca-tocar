308d7bcb73157047e9357244f20c1ed7d237781b
Mario Orostizaga <orostizagamario@gmail.com>
2026-02-14 16:24:55 -0300

    fix: Remove extra closing brace causing syntax error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -16,7 +16,7 @@ const STANDARD_THEMES = [
 
 function generateCode() {
     return Math.random().toString(36).substring(2, 6).toUpperCase();
-}}
+}
 
 // Logout action
 export async function logoutAction() {
